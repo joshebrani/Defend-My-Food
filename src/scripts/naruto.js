b56ab55951a69d25c81c17b092e7830db28d5d5e
your name here <joshuaebrani@gmail.com>
2021-05-19 16:21:41 -0400

    changed HP levels

--- src/scripts/naruto.js
+++ src/scripts/naruto.js
@@ -33,7 +33,7 @@ class Naruto {
         } 
         if (killCount >= 90 && killCount < 150) {
           this.speed = 2;
-          this.health = Math.floor(Math.random() * 2000) + 2500;
+          this.health = Math.floor(Math.random() * 2000) + 1500;
         } 
         if (killCount >= 150) {
             this.speed = 3;
